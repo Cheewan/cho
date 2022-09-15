10210178b136dbc1916f812a377db535f44d39c3
Cheewan <113325619+Cheewan@users.noreply.github.com>
2022-09-15 23:05:31 +0700

    Add files via upload

---
+++ res/access.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+// Users data
+$imSettings['access']['webregistrations_gid'] = 'ldtbikzo';
+$imSettings['access']['users'] = array(
+	'cheewanfix@gmail.com' => array(
+		'groups' => array('s5u1n5nh'),
+		'id' => 'bvbwmeu4',
+		'firstname' => 'Admin',
+		'lastname' => '',
+		'password' => '6MO78lPtq',
+		'email' => 'cheewanfix@gmail.com',
+		'page' => false
+	)
+);
+
+// Admins list
+$imSettings['access']['admins'] = array('bvbwmeu4');
+
+// Page/Users permissions
+$imSettings['access']['pages'] = array();
+
+// End of file access.inc.php
